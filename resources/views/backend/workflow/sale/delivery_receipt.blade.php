62e60b8d02bbf169ad7575964642a79c78916f16
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2017-11-06 18:04:14 +0800

    Reindent

--- resources/views/backend/workflow/sale/delivery_receipt.blade.php
+++ resources/views/backend/workflow/sale/delivery_receipt.blade.php
@@ -70,9 +70,9 @@
 
 <script type="text/javascript">
 function format1(n, currency) {
-    return currency + " " + n.toFixed(2).replace(/./g, function(c, i, a) {
-        return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
-    });
+   return currency + " " + n.toFixed(2).replace(/./g, function(c, i, a) {
+      return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+   });
 }
 
 
