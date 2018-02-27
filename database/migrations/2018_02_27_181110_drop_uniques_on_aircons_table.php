061d48521c59eabe50aa90f656a57474c9964a03
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-02-27 18:11:49 +0800

    Removes uniques

---
+++ database/migrations/2018_02_27_181110_drop_uniques_on_aircons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropUniquesOnAirconsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('aircons', function (Blueprint $table) {
+            $table->dropUnique('serial_number');
+            $table->dropUnique('philippines_serial_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('aircons', function (Blueprint $table) {
+            //
+        });
+    }
+}
